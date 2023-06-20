1c0a86a94ecafc0aa01d78412a7bee1983b2f685
care.nasimakhtar1786@gmail.com <care.nasimakhtar1786.gmail.com>
2023-06-20 15:58:54 +0530

    My Portfolio

---
+++ index.js
@@ -0,0 +1,34 @@
+//--------- About Js-----------
+
+var tablinks = document.getElementsByClassName("tab-link");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+
+//-------- sidemenu ---------
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+
